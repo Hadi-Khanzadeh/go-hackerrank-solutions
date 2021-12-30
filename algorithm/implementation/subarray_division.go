fbe6538da1e62af07db7ec5075be96bccd9e2a0e
Hadi-Khanzadeh <khanzadeh.hadi@gmail.com>
2021-12-30 10:42:07 +0330

    add subarray_division.go

---
+++ algorithm/implementation/subarray_division.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, d, m, c int
+	fmt.Scan(&n)
+	a := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+	fmt.Scan(&d, &m)
+
+	for i := 0; i < n; i++ {
+		length, sum := 0, 0
+		for j := i; j < n; j++ {
+			length++
+			sum += a[j]
+			if sum == d && length == m {
+				c++
+				break
+			}
+			if sum > d || length > m {
+				break
+			}
+		}
+	}
+	fmt.Println(c)
+
+}
